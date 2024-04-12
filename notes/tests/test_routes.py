c5befbcb1d4377b182b81bcc14b97774bb3e43a5
Andrey Anisimov <qwertfy@ya.ru>
2024-04-12 12:04:40 +0300

    Add more tests for rotes.

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -13,17 +13,39 @@ class TestRoutes(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.author = User.objects.create(username='test')
+        cls.author = User.objects.create(username='author')
+        cls.reader = User.objects.create(username='reader')
         cls.note = Note.objects.create(
-            title='Заголовок', text='Текст', slug='test', author=cls.author
+            title='Title', text='Text', slug='text', author=cls.author
         )
 
-    def test_home_page(self):
+    def test_home_page_for_anonymous(self):
         url = reverse('notes:home')
         response = self.client.get(url)
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
-    def test_redirect_for_anonymous_client(self):
+    def test_pages_availability_for_author(self):
+        urls = (
+            ('notes:home', None),
+            ('notes:detail', (self.note.slug,)),
+            ('users:login', None),
+            ('users:logout', None),
+            ('users:signup', None),
+        )
+        self.client.force_login(self.author)    # залогиним автора
+        for name, args in urls:
+            with self.subTest(name=name):
+                url = reverse(name, args=args)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+
+    def test_note_not_available_for_reader(self):
+        url = reverse('notes:detail', kwargs={'slug': self.note.slug})
+        self.client.force_login(self.reader)    # залогиним reader
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+
+    def test_redirects_for_anonymous_client(self):
         url = reverse('notes:detail', kwargs={'slug': self.note.slug})
         login_url = reverse('users:login')
         redirect_url = f'{login_url}?next={url}'
